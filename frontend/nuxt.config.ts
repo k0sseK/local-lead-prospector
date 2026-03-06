f0e852146a6f4909bbe987282355b9f82e77c664
k0sseK <k0ssekcs@gmail.com>
2026-03-06 20:56:21 +0100

    chore(frontend): update Dockerfile and gitignore for Nuxt 4 build

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	compatibilityDate: "2025-01-01",
+	ssr: false,
 	future: {
 		compatibilityVersion: 4,
 	},
